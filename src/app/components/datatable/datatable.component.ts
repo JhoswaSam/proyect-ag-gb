455fc5c472652d04d9f30e0e7aaad86bad57c311
Jhoswa Sam <jhoswasamanez@gmail.com>
2021-10-12 21:24:15 -0500

    Marcelo D - dataTable

---
+++ src/app/components/datatable/datatable.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-datatable',
+  templateUrl: './datatable.component.html',
+})
+export class DatatableComponent implements OnInit{
+  constructor(){
+
+  }
+  ngOnInit(){
+
+  }
+
+}
