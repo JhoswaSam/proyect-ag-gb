73f1476d2a58d1aacb63e4d43658dd26d34b6c6f
Jhoswa Sam <jhoswasamanez@gmail.com>
2021-10-13 13:53:11 -0500

    -.-

--- src/app/components/peliculas/peliculas.component.ts
+++ src/app/components/peliculas/peliculas.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'peliculas',
   templateUrl: './peliculas.component.html',
-  styleUrls: ['./peliculas.component.css']
 })
 export class PeliculasComponent implements OnInit {
 
